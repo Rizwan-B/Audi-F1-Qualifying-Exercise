521710beaa3a29c030c434db3e28a789627b77aa
rizwan-b <rizib44@gmail.com>
2026-06-14 14:31:46 +0100

    Base Structure for Test Cases

---
+++ tests/test_quali.py
@@ -0,0 +1,12 @@
+import sys
+from pathlib import Path
+import pandas as pd
+from qualifying import driver_exists, get_best_laps
+
+sys.path.append(str(Path(__file__).resolve().parents[1] / "src"))
+
+def test_driver_exists():
+    return
+
+def test_best_laps():
+    return

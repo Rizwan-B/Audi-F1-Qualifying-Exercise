fcf73188a91e2fa2f166ab879c1f0a8d6babddef
rizwan-b <rizib44@gmail.com>
2026-06-14 13:13:16 +0100

    Load qualifying dataset
    To see the structure of the dataset.

--- src/main.py
+++ src/main.py
@@ -1,1 +1,14 @@
-print("helloworld")
+import pandas as pd
+
+
+def main():
+    
+    df = pd.read_json("data/session_laptimes.json")
+
+    print(df.head())
+    print()
+    print(df.columns.tolist())
+
+
+if __name__ == "__main__":
+    main()
